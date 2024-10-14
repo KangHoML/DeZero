6d502ce18777b581d96acc99ae7459b64778848b
KangHoML <kanghokhkh1@gmail.com>
2024-10-14 23:18:54 +0900

    Step 5. 역전파 (Backward Pass)

--- chapter1.py
+++ chapter1.py
@@ -3,24 +3,39 @@ import numpy as np
 class Variable:
     def __init__(self, data):
         self.data = data
+        self.grad = None        # 미분값 저장
 
 class Function:
     def __call__(self, input: Variable):
         x = input.data
         y = self.forward(x)
+        self.input = input      # 입력값 저장 (역전파에서 사용)
         output = Variable(y)
         return output
 
     def forward(self, x):
         raise NotImplementedError()
     
+    def backward(self, gy):
+        raise NotImplementedError()
+    
 class Square(Function):
     def forward(self, x):
         return x ** 2
 
+    def backward(self, gy):
+        x = self.input.data
+        gx = 2 * x * gy
+        return gx
+    
 class Exp(Function):
     def forward(self, x):
         return np.exp(x)
+    
+    def backward(self, gy):
+        x = self.input.data
+        gx = np.exp(x) * gy
+        return gx
 
 def numerical_diff(f, x: Variable, eps=1e-4):
     x0 = Variable(x.data - eps) 
