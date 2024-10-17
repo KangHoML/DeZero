67d9640924a503943eba38d4a9486e3dc77b8b20
KangHoML <kanghokhkh1@gmail.com>
2024-10-17 22:06:06 +0900

    step 12. Variable 사용성 개선

--- dezero/core.py
+++ dezero/core.py
@@ -18,19 +18,50 @@ def no_grad():
     return using_config('enable_backprop', False)
 
 class Variable:
-    def __init__(self, data):
+    __array_priority__ = 200
+
+    def __init__(self, data, name=None):
         if data is not None:
             if not isinstance(data, np.ndarray):
                 raise TypeError(f'{type(data)} is not supported')
         
         self.data = data
+        self.name = name
         self.grad = None
         self.creator = None
         self.generation = 0
 
+    def __len__(self):
+        return len(self.data)
+    
+    def __repr__(self):
+        if self.data is None:
+            return 'variable(None)'
+        p = str(self.data).replace('\n', '\n' + ' ' * 9)
+        return 'variable(' + p + ')'
+    
+    @property
+    def shape(self):
+        return self.data.shape
+    
+    @property
+    def ndim(self):
+        return self.data.ndim
+    
+    @property
+    def size(self):
+        return self.data.size
+    
+    @property
+    def dtype(self):
+        return self.data.dtype
+
     def set_creator(self, func):
         self.creator = func
         self.generation = func.generation + 1
+
+    def cleargrad(self):
+        self.grad = None
 
     def backward(self, retain_grad=False):
         if self.grad is None:
@@ -68,11 +99,20 @@ class Variable:
                 for y in f.outputs:
                     y().grad = None
 
-    def cleargrad(self):
-        self.grad = None
+def as_variable(obj):
+    if isinstance(obj, Variable):
+        return obj
+    return Variable(obj)
+    
+def as_array(x):
+    if np.isscalar(x):
+        return np.array(x)
+    return x
 
 class Function:
     def __call__(self, *inputs):
+        inputs = [as_variable(x) for x in inputs]
+
         xs = [x.data for x in inputs]
         ys = self.forward(*xs)
         if not isinstance(ys, tuple):
@@ -94,7 +134,94 @@ class Function:
     def backward(self, gys):
         raise NotImplementedError()
 
-def as_array(x):
-    if np.isscalar(x):
-        return np.array(x)
-    return x
+class Add(Function):
+    def forward(self, x0, x1):
+        return x0 + x1
+
+    def backward(self, gy):
+        return gy, gy
+
+def add(x0, x1):
+    x1 = as_array(x1)
+    return Add()(x0, x1)
+
+class Mul(Function):
+    def forward(self, x0, x1):
+        return x0 * x1
+
+    def backward(self, gy):
+        x0, x1 = self.inputs[0].data, self.inputs[1].data
+        return gy * x1, gy * x0
+
+def mul(x0, x1):
+    x1 = as_array(x1)
+    return Mul()(x0, x1)
+
+class Sub(Function):
+    def forward(self, x0, x1):
+        return x0 - x1    
+    
+    def backward(self, gy):
+        return gy, -gy
+    
+def sub(x0, x1):
+    x1 = as_array(x1)
+    return Sub()(x0, x1)
+
+def rsub(x0, x1):
+    x1 = as_array(x1)
+    return Sub()(x1, x0)
+
+class Div(Function):
+    def forward(self, x0, x1):
+        return x0 / x1
+
+    def backward(self, gy):
+        x0, x1 = self.inputs[0].data, self.inputs[1].data
+        gx0 = gy / x1
+        gx1 = gy * (-x0 / x1 ** 2)
+        return gx0, gx1
+
+def div(x0, x1):
+    x1 = as_array(x1)
+    return Div()(x0, x1)
+
+def rdiv(x0, x1):
+    x1 = as_array(x1)
+    return Div()(x1, x0)
+
+class Neg(Function):
+    def forward(self, x):
+        return -x
+    
+    def backward(self, gy):
+        return -gy
+
+def neg(x):
+    return Neg()(x)
+
+class Pow(Function):
+    def __init__(self, c):
+        self.c = c
+
+    def forward(self, x):
+        return x ** self.c
+    
+    def backward(self, gy):
+        x, = self.inputs
+        c = self.c
+        return c * x ** (c - 1) * gy
+
+def pow(x, c):
+    return Pow(c)(x)
+
+Variable.__add__ = add
+Variable.__radd__ = add
+Variable.__mul__ = mul
+Variable.__rmul__ = mul
+Variable.__sub__ = sub
+Variable.__rsub__ = rsub
+Variable.__truediv__ = div
+Variable.__rtruediv__ = rdiv
+Variable.__neg__ = neg
+Variable.__pow__ = pow
--- dezero/functions.py
+++ dezero/functions.py
@@ -24,14 +24,3 @@ class Exp(Function):
 
 def exp(x):
     return Exp()(x)
-
-class Add(Function):
-    def forward(self, x0, x1):
-        y = x0 + x1
-        return y
-
-    def backward(self, gy):
-        return gy, gy
-
-def add(x0, x1):
-    return Add()(x0, x1)
