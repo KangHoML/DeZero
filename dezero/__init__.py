84a9dc0cb33c944f3f5a2715ddbe8c7b14890ca6
KangHoML <kanghokhkh1@gmail.com>
2024-10-17 22:12:13 +0900

    step 13. 패키징

--- dezero/__init__.py
+++ dezero/__init__.py
@@ -1,7 +1,13 @@
 from dezero.core import Variable
 from dezero.core import Function
+from dezero.core import using_config
+from dezero.core import no_grad
+from dezero.core import as_array
+from dezero.core import as_variable
+from dezero.core import setup_variable
 
 import dezero.utils
 import dezero.functions
 
 __version__ = '0.0.1'
+setup_variable()
--- dezero/core.py
+++ dezero/core.py
@@ -215,13 +215,14 @@ class Pow(Function):
 def pow(x, c):
     return Pow(c)(x)
 
-Variable.__add__ = add
-Variable.__radd__ = add
-Variable.__mul__ = mul
-Variable.__rmul__ = mul
-Variable.__sub__ = sub
-Variable.__rsub__ = rsub
-Variable.__truediv__ = div
-Variable.__rtruediv__ = rdiv
-Variable.__neg__ = neg
-Variable.__pow__ = pow
+def setup_variable():
+    Variable.__add__ = add
+    Variable.__radd__ = add
+    Variable.__mul__ = mul
+    Variable.__rmul__ = mul
+    Variable.__sub__ = sub
+    Variable.__rsub__ = rsub
+    Variable.__truediv__ = div
+    Variable.__rtruediv__ = rdiv
+    Variable.__neg__ = neg
+    Variable.__pow__ = pow
