2e88cf5939a2baf5c6cec1425c96635df395e84c
KangHoML <kanghokhkh1@gmail.com>
2024-10-21 16:28:32 +0900

    step 18. sin / cos / tanh

--- dezero/functions.py
+++ dezero/functions.py
@@ -1,25 +1,53 @@
 import numpy as np
 from dezero.core import Variable, Function
 
-class Square(Function):
+class Sin(Function):
     def forward(self, x):
-        return x ** 2
-
+        y = np.sin(x)
+        return y
+    
     def backward(self, gy):
-        x = self.inputs[0].data
-        gx = 2 * x * gy
+        x, = self.inputs
+        gx = gy * cos(x)
         return gx
 
-def square(x):
-    return Square()(x)
+def sin(x):
+    return Sin()(x)
+
+class Cos(Function):
+    def forward(self, x):
+        y = np.cos(x)
+        return y
+    
+    def backward(self, gy):
+        x, = self.inputs
+        gx = gy * -sin(x)
+        return gx
+
+def cos(x):
+    return Cos()(x)
+
+class Tanh(Function):
+    def forward(self, x):
+        y = np.tanh(x)
+        return y
+    
+    def backward(self, gy):
+        y = self.outputs[0]()
+        gx = gy * (1 - y ** 2)
+        return gx
+
+def tanh(x):
+    return Tanh()(x)
 
 class Exp(Function):
     def forward(self, x):
-        return np.exp(x)
+        y = np.exp(x)
+        return y
     
     def backward(self, gy):
-        x = self.inputs[0].data
-        gx = np.exp(x) * gy
+        y = self.outputs[0]()
+        gx = gy * y
         return gx
 
 def exp(x):
